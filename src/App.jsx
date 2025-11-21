e96920b11f8943467bb7be0f9e8101d162ed5a00
ArafatSany7 <human.sany7@gmail.com>
2025-11-22 02:05:19 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl bg-amber-400 flex justify-center items-center">
+        Vite + React
+      </h1>
+    </>
+  );
+}
+
+export default App;
