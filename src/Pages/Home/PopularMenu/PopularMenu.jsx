6d884ecc20f9e53a7572ee5d7ff231084dfd3338
ArafatSany7 <human.sany7@gmail.com>
2025-11-23 15:15:09 +0600

    home-done

---
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import SectionTittle from "../../../Components/SectionTittle/SectionTittle";
+import MenuItem from "../../Shared/MenuItem/MenuItem";
+
+const PopularMenu = () => {
+  const [menu, setMenu] = useState([]);
+
+  useEffect(() => {
+    fetch("Menu.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const popularItems = data.filter((item) => item.category === "popular");
+        setMenu(popularItems);
+      });
+  }, []);
+
+  return (
+    <div>
+      <SectionTittle
+        heading={"From Our Menu"}
+        subHeading={"Popular Item"}
+      ></SectionTittle>
+      <div className="grid md:grid-cols-2 gap-4 mb-5 gap-7">
+        {menu.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PopularMenu;
