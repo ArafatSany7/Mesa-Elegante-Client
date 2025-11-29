b688fe50bdc25e4bb6e52377300d5d0010bf2470
ArafatSany7 <human.sany7@gmail.com>
2025-11-30 02:34:43 +0600

    65-2

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,4 +1,7 @@
 import { Helmet } from "react-helmet-async";
+import Cover from "../../Shared/Cover/Cover";
+import menuimg from "../../../../../assets/menu/banner3.jpg";
+import PopularMenu from "./../../Home/PopularMenu/PopularMenu";
 
 const Menu = () => {
   return (
@@ -6,6 +9,12 @@ const Menu = () => {
       <Helmet>
         <title>Mesa Elegante | Menu</title>
       </Helmet>
+      <Cover img={menuimg} tittle={"Our Menu"}></Cover>
+      <PopularMenu></PopularMenu>
+      <Cover img={menuimg} tittle={"Our Menu"}></Cover>
+      <PopularMenu></PopularMenu>
+      <Cover img={menuimg} tittle={"Our Menu"}></Cover>
+      <PopularMenu></PopularMenu>
     </div>
   );
 };
